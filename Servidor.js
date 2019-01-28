a19be2c2fd794914d138cd6563801ebe23380bcd
angelzarazua <duce13@live.com.mx>
2019-01-27 21:05:18 -0600

    Subida 1

---
+++ Servidor.js
@@ -0,0 +1,48 @@
+var express = require('express')
+var app = express()
+
+var server = require('http').Server(app)
+var io  = require('socket.io')(server)
+var os = require('os')
+var net = require('net')
+
+var interfaces = os.networkInterfaces;
+var address = [];
+
+for (var k in interfaces) {
+    for(var k2 in interfaces[k]){
+        var address = interfaces[k][k2];
+        if(address.family == 'IPv4' && !address.internal()){
+            console.log(address.address)
+            addresses.push(address.address)
+        }
+    }
+}
+
+var HOST = addresses[0]
+var PORT = '4444'
+var sockets = [];
+
+var ser = net.createServer(function(){
+    sockets[0] = so;
+    sockets.write(sockets[0]);
+    console.log(so);
+    console.log('Usuario conectado'+ so.remoteAddress+':'+so.remotePort)
+
+})
+
+io.on('connection', function(socket){
+    console.log("Conectado"+socket)
+})
+
+server.listen(PORT, function() {
+    console.log("Servidor  Activo " + PORT)
+})
+
+net.createServer(function(sock){
+    console.log('Conectado: ' + sock.remoteAddress + ':' + sock.remotePort)
+    sock.on('data', function(data) {
+        console.log(data)
+    })
+}).listen(PORT, HOST)
+
